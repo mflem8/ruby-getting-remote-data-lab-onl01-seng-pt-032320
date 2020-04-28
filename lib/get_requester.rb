07cdb1ccaaeeed3e7e6b313bd40da29e800a10d8
Matt Fleming <matthewfleming8@gmail.com>
2020-04-28 00:25:11 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,7 +1,21 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require json
+
 class GetRequester
 
-  def initialize(url)
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+  def get_response_body
+    #sends a GET request to the URL passed in on initialization. This method
+    #should return the body of the response
   end
+
+  def parse_json 
+    #use get_response_body to send a request, then return a Ruby Array or
+    #Hash made up of data converted from the response of that request
+  end
+  
 
 end
