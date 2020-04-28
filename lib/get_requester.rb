900a1bb6fbce80ad7303df321f35fb7dcb7efffd
Matt Fleming <matthewfleming8@gmail.com>
2020-04-28 00:30:19 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,6 +4,9 @@ require 'open-uri'
 require 'json'
 
 class GetRequester
+  def initialize(url)
+    @url = url 
+  end
 
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
