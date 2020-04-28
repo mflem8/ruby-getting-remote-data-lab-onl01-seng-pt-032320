9ce89a6c57ce17bd9274d34f5ae18933593f5e64
Matt Fleming <matthewfleming8@gmail.com>
2020-04-28 00:28:24 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -9,13 +9,17 @@ class GetRequester
 
   def get_response_body
     uri = URI.parse(URL)
-    response = NET::HTTP.get_response(uri)
+    response = NET::HTTP.get_response_body(uri)
     response.body
     #sends a GET request to the URL passed in on initialization. This method
     #should return the body of the response
   end
 
   def parse_json
+    parse = JSON.parse(self.get_response_body)
+      parse.collect do |body|
+        parse[" "]
+      end
     #use get_response_body to send a request, then return a Ruby Array or
     #Hash made up of data converted from the response of that request
   end
